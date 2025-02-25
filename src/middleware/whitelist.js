6947ca7433e30a89e3fe07bb7862d00897774d75
Stephen Brown <steve@fig14.com>
2025-02-25 16:33:00 +0000

    Show forwarded IP in whitelist denied message

--- src/middleware/whitelist.js
+++ src/middleware/whitelist.js
@@ -75,7 +75,7 @@ export default function whitelistMiddleware() {
             if (!noLogPaths.includes(req.path)) {
                 console.warn(
                     color.red(
-                        `Blocked connection from ${clientIp}; User Agent: ${userAgent}\n\tTo allow this connection, add its IP address to the whitelist or disable whitelist mode by editing config.yaml in the root directory of your SillyTavern installation.\n`,
+                        `Blocked connection from ${ipDetails}; User Agent: ${userAgent}\n\tTo allow this connection, add its IP address to the whitelist or disable whitelist mode by editing config.yaml in the root directory of your SillyTavern installation.\n`,
                     ),
                 );
             }
