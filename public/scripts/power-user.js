68b5be063ffa5281aa5968672865a7af3157e8d9
Cohee <18619528+Cohee1207@users.noreply.github.com>
2025-03-11 00:24:28 +0200

    TC: Increased unlocked response to 32k

--- public/scripts/power-user.js
+++ public/scripts/power-user.js
@@ -71,7 +71,7 @@ export {
 export const MAX_CONTEXT_DEFAULT = 8192;
 export const MAX_RESPONSE_DEFAULT = 2048;
 const MAX_CONTEXT_UNLOCKED = 200 * 1024;
-const MAX_RESPONSE_UNLOCKED = 16 * 1024;
+const MAX_RESPONSE_UNLOCKED = 32 * 1024;
 const unlockedMaxContextStep = 512;
 const maxContextMin = 512;
 const maxContextStep = 64;
