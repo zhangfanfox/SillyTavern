449522c3aa7f4d4c8b66e7db60e2834fa532d96c
Cohee <18619528+Cohee1207@users.noreply.github.com>
2025-05-23 20:01:53 +0300

    Combine all toastr settings into one object

--- public/script.js
+++ public/script.js
@@ -315,30 +315,26 @@ await new Promise((resolve) => {
 
 // Configure toast library:
 toastr.options = {
-    'closeButton': false,
-    'progressBar': false,
-    'showDuration': 250,
-    'hideDuration': 250,
-    'timeOut': 4000,
-    'extendedTimeOut': 10000,
-    'showEasing': 'linear',
-    'hideEasing': 'linear',
-    'showMethod': 'fadeIn',
-    'hideMethod': 'fadeOut',
-    'escapeHtml': true,
-};
-
-
-toastr.options.onHidden = () => {
-    // If we have any dialog still open, the last "hidden" toastr will remove the toastr-container. We need to keep it alive inside the dialog though
-    // so the toasts still show up inside there.
-    fixToastrForDialogs();
-};
-
-toastr.options.onShown = function () {
-    const $toast = $(this); // 'this' refers to the toast element
-    const message = 'Click/Tap to close';
-    $toast.attr('title', message); // Set tooltip to the notification message
+    closeButton: false,
+    progressBar: false,
+    showDuration: 250,
+    hideDuration: 250,
+    timeOut: 4000,
+    extendedTimeOut: 10000,
+    showEasing: 'linear',
+    hideEasing: 'linear',
+    showMethod: 'fadeIn',
+    hideMethod: 'fadeOut',
+    escapeHtml: true,
+    onHidden: function () {
+        // If we have any dialog still open, the last "hidden" toastr will remove the toastr-container. We need to keep it alive inside the dialog though
+        // so the toasts still show up inside there.
+        fixToastrForDialogs();
+    },
+    onShown: function () {
+        // Set tooltip to the notification message
+        $(this).attr('title', t`Tap to close`);
+    },
 };
 
 // Allow target="_blank" in links
